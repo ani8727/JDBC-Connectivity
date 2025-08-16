a571b8f074caccf53af5c6838b6839380fc12620
ani8727 <aniketgupta.8727@gmail.com>
2025-08-16 19:04:38 +0530

    Create Connection - driver laod

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+/*
+ Flow of connect JDBC
+ 1. Connect IDE with a necessary Connector - jar file in project structure
+
+ 2. Create Connection - driver laod
+ */
